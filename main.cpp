5779aaf8a51bc1ea959c2134c62d7dc5e45d30d8
frazzex <rtmtolokonnikov@gmail.com>
2026-04-05 23:20:06 +0700

    [ADD] Решение 2 задания

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "task1/Complex.h"
+#include "task2/Rational.h"
 
 using namespace std;
 
@@ -40,10 +41,57 @@ void task1() {
     cout << "complex1 != complex2 : " << (complex1 != complex2 ? "Да" : "Нет") << endl;
 }
 
+void task2() {
+    Rational rational1 = {8, 3};
+    Rational rational2 = {11, 4};
+
+    cout << "----- Изначальные рациональные числа -----" << endl;
+    cout << "Число 1: " << rational1 << endl;
+    cout << "Число 2: " << rational2 << endl << endl;
+
+    cout << "----- Арифметические операции над дробями -----" << endl;
+    cout << "Сложение: " << rational1 << " + " << rational2 << " = " << rational1 + rational2 << endl;
+    cout << "Вычитание: " << rational1 << " - " << rational2 << " = " << rational1 - rational2 << endl;
+    cout << "Умножение: " << rational1 << " * " << rational2 << " = " << rational1 * rational2 << endl;
+    cout << "Деление: " << rational1 << " / " << rational2 << " = " << rational1 / rational2 << endl;
+
+    cout << "----- Арифметические операции над  рац. числом с использованием int-чисел -----" << endl;
+    cout << "Сложение с числом 5: " << rational1 << " + 5 = " << rational1 + 5 << endl;
+    cout << "Вычитание с числом 5: " << rational1 << " - 5 = " << rational1 - 5 << endl;
+    cout << "Умножение на число 5: " << rational1 << " * 5 = " << rational1 * 5 << endl;
+    cout << "Деление на число 5: " << rational1 << " / 5 = " << rational1 / 5 << endl << endl;
+
+    cout << "----- Составные операторы присваивания -----" << endl;
+
+    cout << "Сложение:" << rational1 << " += " << rational2 << " = ";
+    rational1 += rational2;
+    cout << rational1 << endl;
+
+    cout << "Вычитание:" << rational1 << " -= " << rational2 << " = ";
+    rational1 -= rational2;
+    cout << rational1 << endl;
+
+    cout << "Умножение:" << rational1 << " *= " << rational2 << " = ";
+    rational1 *= rational2;
+    cout << rational1 << endl;
+
+    cout << "Деление:" << rational1 << " /= " << rational2 << " = ";
+    rational1 /= rational2;
+    cout << rational1 << endl << endl;
+
+    cout << "----- Операторы сравнения -----" << endl;
+    cout << rational1 << " == " << rational2 << "? Ответ: " << (rational1 == rational2 ? "Да" : "Нет") << endl;
+    cout << rational1 << " != " << rational2 << "? Ответ: " << (rational1 != rational2 ? "Да" : "Нет") << endl << endl;
+
+    cout << "----- Унарные операторы -----" << endl;
+    cout << "Оператор + : +" << rational1 << " = " << +rational1 << endl;
+    cout << "Оператор - : -" << rational1 << " = " << -rational1 << endl;
+}
+
 int main() {
     system("chcp 65001");
     setlocale(LC_ALL, "ru");
-    task1();
+    task2();
 
     return 0;
 }
